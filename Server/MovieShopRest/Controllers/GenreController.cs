f6144fafc8d12d5142bf2ff0c63a23ac69a70d22
Miztah87 <zalantassaly@gmail.com>
2015-11-18 12:42:57 +0100

    Genre Controlleerrrr!!!!!!!!!!!

---
+++ Server/MovieShopRest/Controllers/GenreController.cs
@@ -0,0 +1,51 @@
+﻿
+using MovieShopDAL;
+using MovieShopDAL.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GenreShopRest.Controllers
+{
+    public class GenreController : ApiController
+    {
+        private Facade facade = new Facade();
+        public IEnumerable<Genre> GetGenres()
+        {
+            return facade.GetGenresRepository().ReadAll();
+        }
+
+        public Genre GetGenre(int id)
+        {
+            return facade.GetGenresRepository().Find(id);
+        }
+
+
+        public void PostGenre(Genre Genre)
+        {
+            facade.GetGenresRepository().Add(Genre);
+
+        }
+
+        public void DeleteGenre(int id)
+        {
+            Genre Genre = facade.GetGenresRepository().Find(id);
+            if (Genre == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            facade.GetGenresRepository().Delete(id);
+        }
+
+        public void PutGenre(int id, Genre Genre)
+        {
+            Genre.Id = id;
+            facade.GetGenresRepository().Edit(Genre);
+
+        }
+
+    }
+}
