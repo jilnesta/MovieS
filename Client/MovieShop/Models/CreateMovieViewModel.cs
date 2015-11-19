a5acc66445439fec83de44b747eb39c36fa21821
kriahja <kriahja@gmail.com>
2015-11-19 14:28:11 +0100

    Genre working! yayy

---
+++ Client/MovieShop/Models/CreateMovieViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DtoModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieShop.Models
+{
+    public class CreateMovieViewModel
+    {
+        public Movie Movie { get; set; }
+
+        public MultiSelectList Genres { get; set; }
+        public List<int> SelectedGenres { get; set; }
+    }
+}
